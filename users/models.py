a35d65cdd7fc423a5cb8dc54f0ce53781032d7b1
Miguel Ramos <return.infox@gmail.com>
2021-10-18 06:51:42 -0300

    first commit

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'# para hacer login con email
+    REQUIRED_FIELDS = [] # Es necesario cada vez que usamos el  USERNAME_FIELD
